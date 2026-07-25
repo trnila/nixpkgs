66dec108bc41a5489560516dd15f08c1635c7416
Daniel Trnka <daniel.trnka@gmail.com>
2026-07-25 19:30:18 +0200

    update

--- pkgs/by-name/te/tensorflow-lite/package.nix
+++ pkgs/by-name/te/tensorflow-lite/package.nix
@@ -39,7 +39,7 @@ buildBazelPackage rec {
     owner = "tensorflow";
     repo = "tensorflow";
     rev = "v${version}";
-    hash = "sha256-xq5pAVmxlWBVnph20fkAwbfy+iuBNlfFy14poDPd5h0=";
+    hash = "sha256-Hs3g80wSHex1ejz7H8eu6MJMzwthx58sPGDh/dG66FQ=";
   };
 
   bazel = buildPackages.bazel;
